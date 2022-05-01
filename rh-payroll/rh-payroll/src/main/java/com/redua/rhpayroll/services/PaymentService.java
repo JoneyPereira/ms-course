70f6f0c58a236765ae7e09a559873241f64b7f3a
Joney Pereira <joney.pereira.ext@conductor.com.br>
2022-05-01 19:46:51 -0300

    Implemntação do RestTemplate na folha de pagamento

--- rh-payroll/rh-payroll/src/main/java/com/redua/rhpayroll/services/PaymentService.java
+++ rh-payroll/rh-payroll/src/main/java/com/redua/rhpayroll/services/PaymentService.java
@@ -1,13 +1,30 @@
 package com.redua.rhpayroll.services;
 
 import com.redua.rhpayroll.entities.Payment;
+import com.redua.rhpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("S{hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(long workerId, int days){
 
-        return new Payment("Joney", 150.00, days);
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("id", ""+workerId);
+
+        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
